3bfc207ce01c54286bb45cb91fada4913384fb7f
YoungOldMan <Timonve@mail.ru>
2021-02-25 15:54:16 +0300

    1

--- src/com/Main.java
+++ src/com/Main.java
@@ -6,44 +6,43 @@ import java.util.Stack;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
             String checkedString;
-            while ((checkedString = reader.readLine()) != "") {
+            while (!(checkedString = reader.readLine()).equals("")){ // считываем строки из консоли до тех пор, пока не будет введена пустая строка
                 System.out.println(check(checkedString));
             }
         }catch (IOException e){
-
+            System.out.println(e);
         }
     }
 
     static boolean check (String checkedString){
-        Stack<Character> stack = new Stack<>();
-        for (char c: checkedString.toCharArray()){
+        Stack<Character> stack = new Stack<>(); // стек в котором хранятся открывающие скобки
+        for (char c: checkedString.toCharArray()){ // проверяем каждый символ строки
             switch (c){
                 case '{':
                 case '[':
                 case '(':
-                    stack.push(c);
+                    stack.push(c); // если текущий символ открывающая скобка заносим её в стек
                     break;
-                case '}':
-                    if (stack.empty() || stack.pop() != '{') {
+                case '}': // если текущий символ закрывающая скобка выполняем проверку
+                    if (stack.empty() || stack.pop() != '{') { // если стек пустой (отсутсвуют открывающие скобки) или последняя извлеченная открывающая скобка не соответсвует закрывающие - возвращаем false
                         return false;
                     }
                     break;
                 case ']':
-                    if (stack.empty() || stack.pop() != '[') {
+                    if (stack.empty() || stack.pop() != '[') { // если стек пустой (отсутсвуют открывающие скобки) или последняя извлеченная открывающая скобка не соответсвует закрывающие - возвращаем false
                         return false;
                     }
                     break;
                 case ')':
-                    if (stack.empty() || stack.pop() != '(') {
+                    if (stack.empty() || stack.pop() != '(') { // если стек пустой (отсутсвуют открывающие скобки) или последняя извлеченная открывающая скобка не соответсвует закрывающие - возвращаем false
                         return false;
                     }
                     break;
             }
         }
-        if (!stack.empty()){
+        if (!stack.empty()){ // если по окончанию перебора всех символов стек не пустой (остались не закрытые скобки) - возвращаем false
             return false;
         }
         return true;
